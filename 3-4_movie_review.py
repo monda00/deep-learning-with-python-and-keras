3cc3484faf80985efeaa2ea0e317f9c038edc98f
monda <monda0524@gmail.com>
2019-03-26 21:15:24 +0900

    update 3-4 3 layers

--- 3-4_movie_review.py
+++ 3-4_movie_review.py
@@ -68,7 +68,8 @@ def show_acc_history(history):
 def define_model():
     model = models.Sequential()
     model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
-    # model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense(16, activation='relu'))
     model.add(layers.Dense(1, activation='sigmoid'))
 
     # model.compile(optimizer='rmsprop',
