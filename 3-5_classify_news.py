340c741dc16ff488017238cbbd6226a581699d7b
monda <monda0524@gmail.com>
2019-03-27 23:18:07 +0900

    update 3-5 unit is 128

--- 3-5_classify_news.py
+++ 3-5_classify_news.py
@@ -67,7 +67,7 @@ def show_acc_history(history):
 def define_model():
     model = models.Sequential()
     model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
-    model.add(layers.Dense(4, activation='relu'))
+    model.add(layers.Dense(128, activation='relu'))
     model.add(layers.Dense(46, activation='softmax'))
 
     model.compile(optimizer='rmsprop',
