d41ac4d8913564dbf983e59e68dd38541a039514
monda <monda0524@gmail.com>
2019-03-27 22:53:09 +0900

    update 3-5 add fit and show history

--- 3-5_classify_news.py
+++ 3-5_classify_news.py
@@ -7,6 +7,7 @@ from keras import models
 from keras import layers
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def print_data(data, labels):
     print(len(data))
@@ -34,6 +35,35 @@ def to_one_hot(labels, dimension=46):
         results[i, label] = 1.
     return results
 
+def show_loss_history(history):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(loss) + 1)
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Trainig and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+def show_acc_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Trainig and validation acc')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+
 def define_model():
     model = models.Sequential()
     model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
@@ -59,6 +89,19 @@ if __name__ == '__main__':
     one_hot_train_labels = to_one_hot(train_labels)
     one_hot_test_labels = to_one_hot(test_labels)
 
+    # 検証データの設定
+    x_val = x_train[:1000]
+    partial_x_train = x_train[1000:]
+    y_val = one_hot_train_labels[:1000]
+    partial_y_train = one_hot_train_labels[1000:]
+
     model = define_model()
+
+    history = model.fit(partial_x_train, partial_y_train,
+                        epochs=20, batch_size=512,
+                        validation_data=(x_val, y_val))
+
+    show_loss_history(history)
+    show_acc_history(history)
 
 
