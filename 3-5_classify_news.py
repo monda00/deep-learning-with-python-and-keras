2565027e26d14dfd0de08f4482e2fa403b9eb4d8
monda <monda0524@gmail.com>
2019-03-27 23:20:51 +0900

    update 3-5 one layer

--- 3-5_classify_news.py
+++ 3-5_classify_news.py
@@ -67,7 +67,7 @@ def show_acc_history(history):
 def define_model():
     model = models.Sequential()
     model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
-    model.add(layers.Dense(128, activation='relu'))
+    # model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dense(46, activation='softmax'))
 
     model.compile(optimizer='rmsprop',
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     model = define_model()
 
     history = model.fit(partial_x_train, partial_y_train,
-                        epochs=20, batch_size=128,
+                        epochs=8, batch_size=128,
                         validation_data=(x_val, y_val))
 
     show_loss_history(history)
