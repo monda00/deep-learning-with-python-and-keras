5d5d8ee0998fc91f6590cfaf250cf371739b6e1f
monda <monda0524@gmail.com>
2019-03-27 21:42:27 +0900

    update 3-5 add to_words

--- 3-5_classify_news.py
+++ 3-5_classify_news.py
@@ -8,11 +8,20 @@ def print_data(data, labels):
     print(len(data))
     print(data[8])
 
+def to_words(train_data):
+    word_index = reuters.get_word_index()
+    reverse_word_index = \
+        dict([(value, key) for (key, value) in word_index.items()])
+
+    decoded_newswire = ' '.join(
+        [reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+    print(decoded_newswire)
+
 
 if __name__ == '__main__':
     # データの読み込み
     (train_data, train_labels), (test_data, test_labels) = \
         reuters.load_data(num_words=10000)
 
-    print_data(train_data, train_labels)
+    to_words(train_data)
 
