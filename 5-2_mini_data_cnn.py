1e73b988d7a08b8b617af721a70302d36d3e8749
monda <monda0524@gmail.com>
2019-04-08 18:55:56 +0900

    add 5-2 build model

--- 5-2_mini_data_cnn.py
+++ 5-2_mini_data_cnn.py
@@ -3,6 +3,9 @@
 '''
 
 import os, shutil
+from keras import layers
+from keras import models
+from keras import optimizers
 
 def make_dataset():
     # 元のデータセットを展開したディレクトリへのパス
@@ -85,7 +88,31 @@ def make_dataset():
         dst = os.path.join(test_dogs_dir, fname)
         shutil.copyfile(src, dst)
 
+def build_cnn_model():
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(150, 150, 1)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer=optimizers.RMSprop(lr=1e-4),
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    return model
+
 
 if __name__ == '__main__':
-    make_dataset()
+    # make_dataset()
+
+    model = build_cnn_model()
+    model.summary()
 
