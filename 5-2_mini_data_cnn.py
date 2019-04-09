98cef547f1b4b3e601bbf5026db52b14a1de91f1
monda <monda0524@gmail.com>
2019-04-09 21:08:47 +0900

    update 5-2 add extended data method

--- 5-2_mini_data_cnn.py
+++ 5-2_mini_data_cnn.py
@@ -8,6 +8,7 @@ from keras import layers
 from keras import models
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
 
 def make_dataset():
     # 元のデータセットを展開したディレクトリへのパス
@@ -90,7 +91,7 @@ def make_dataset():
     #     dst = os.path.join(test_dogs_dir, fname)
     #     shutil.copyfile(src, dst)
 
-    return train_dir, validation_dir
+    return train_dir, validation_dir, train_cats_dir
 
 def build_cnn_model():
     model = models.Sequential()
@@ -156,9 +157,48 @@ def show_loss_and_acc(history):
 
     plt.savefig('./fig/loss.png')
 
+def extended_data():
+    datagen = ImageDataGenerator(rotation_range=40,
+                                 width_shift_range=0.2,
+                                 height_shift_range=0.2,
+                                 shear_range=0.2,
+                                 zoom_range=0.2,
+                                 horizontal_flip=True,
+                                 fill_mode='nearest')
+
+    return datagen
+
+def show_extended_data(datagen, train_cats_dir):
+    fnames = [os.path.join(train_cats_dir, fname)
+              for fname in os.listdir(train_cats_dir)]
+
+    # 水増しする画像を選択
+    img_path = fname[3]
+
+    # 画像を読み込み、サイズを変更
+    img = image.load_img(img_path, target_size=(150, 150))
+
+    # 形状が(150, 150, 3)のNumPy配列に変換
+    x = image.img_to_array(img)
+
+    # (1, 150, 150, 3)に変形
+    x = x.reshape((1, ) + x.shape)
+
+    # ランダムに変換した画像のパッチを生成する
+    # 無限ループとなるため、何らかのタイミングでbreakする必要がある
+    i = 0
+    for batch in datagen.flow(x, batch_size=1):
+        plt.figure(i)
+        imgplot = plt.imshow(image.array_to_img(batch[0]))
+        i += 1
+        if i % 4 == 0:
+            break
+
+    plt.show()
+
 
 if __name__ == '__main__':
-    train_dir, validation_dir = make_dataset()
+    train_dir, validation_dir, train_cats_dir = make_dataset()
 
     model = build_cnn_model()
     model.summary()
