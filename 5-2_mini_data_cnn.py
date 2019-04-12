a1b6e5c98844a7a9c27a8dfabaf439766783c4cf
monda <monda0524@gmail.com>
2019-04-12 22:33:37 +0900

    udpate 5-2 extract features

--- 5-2_mini_data_cnn.py
+++ 5-2_mini_data_cnn.py
@@ -4,6 +4,7 @@
 
 import os, shutil
 import matplotlib.pyplot as plt
+import numpy as np
 from keras import layers
 from keras import models
 from keras import optimizers
@@ -266,11 +267,53 @@ def training_model_from_scratch():
 
     show_loss_and_acc(history)
 
+def extract_features(directory, sample_count, batch_size, conv_base, datagen):
+    features = np.zeros(shape=(sample_count, 4, 4, 512))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(directory,
+                                            target_size=(150, 150),
+                                            batch_size=batch_size,
+                                            class_mode='binary')
+
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+            # ジェネレータはデータを無限ループで生成するため
+            # 画像を一通り処理したらbreakしなければならない
+            break
+
+    return features, labels
+
 def training_model_from_trained_model():
+    base_dir = './data/cats_and_dogs_small'
+
+    train_dir = os.path.join(base_dir, 'train')
+    validation_dir = os.path.join(base_dir, 'validation')
+    test_dir = os.path.join(base_dir, 'test')
+
+    datagen = ImageDataGenerator(rescale=1./255)
+    batch_size=20
+
     conv_base = VGG16(weights='imagenet',
                       include_top=False,
                       input_shape=(150, 150, 3))
     conv_base.summary()
+
+    train_features, train_labels = extract_features(train_dir, 2000, batch_size,
+                                                    conv_base, datagen)
+    validation_features, validation_labels = extract_features(validation_dir,
+                                                              1000, conv_base,
+                                                              datagen)
+    test_features, test_labels = extract_features(test_dir, 1000, conv_base,
+                                                  datagen)
+
+    train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+    validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
+    test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
 
 
 if __name__ == '__main__':
