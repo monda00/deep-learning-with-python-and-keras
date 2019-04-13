6a03bbf145df13ab45f07b680d43675208c2f75d
monda <monda0524@gmail.com>
2019-04-13 21:52:56 +0900

    update 5-2 extracting features with extend data

--- 5-2_mini_data_cnn.py
+++ 5-2_mini_data_cnn.py
@@ -324,6 +324,15 @@ def show_loss_and_acc_trained(history):
     plt.legend()
     plt.savefig('./fig/loss_trained.png')
 
+def build_extend_conv_base_model(conv_base):
+    model = models.Sequential()
+    model.add(conv_base)
+    model.add(layers.Flatten())
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    conv_base.trainable = False
+
 def training_model_from_trained_model():
     base_dir = './data/cats_and_dogs_small'
 
@@ -358,6 +367,48 @@ def training_model_from_trained_model():
                         epochs=30,
                         batch_size=20,
                         validation_data=(validation_features, validation_labels))
+
+def training_model_from_trained_model_with_extend_data():
+    conv_base = VGG16(weights='imagenet',
+                      include_top=False,
+                      input_shape=(150, 150, 3))
+
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,)
+
+    # 検証データは水増しするべきではないことに注意
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+    validation_generator = test_datagen.flow_from_directory(
+        validation_dir,
+        target_size=(150, 150),
+        batch_size=32,
+        class_mode='binary')
+
+    model = build_extend_conv_base_model(conv_base)
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optimizers.RMSprop(lr=2e-5),
+                  metrics=['acc'])
+
+    history = model.fit_generator(train_generator,
+                                  steps_per_epoch=100,
+                                  epochs=30,
+                                  validation_data=validation_generator,
+                                  validation_steps=50,
+                                  verbose=2)
 
 
 if __name__ == '__main__':
