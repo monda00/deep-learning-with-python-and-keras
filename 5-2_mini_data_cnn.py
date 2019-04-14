6af8c90bdfda57ded1b5320c748c1bc9ba079875
monda <monda0524@gmail.com>
2019-04-14 17:40:25 +0900

    update 5-2 show activations

--- 5-2_mini_data_cnn.py
+++ 5-2_mini_data_cnn.py
@@ -502,7 +502,72 @@ def showing_middle_layer_output():
     plt.matshow(first_layer_activation[0, :, :, 3], cmap='viridis')
     plt.show()
 
+def showing_middle_layer_output_all():
+    model = load_model('cats_and_dogs_small_2.h5')
+    model.summary()
+
+    img_path = \
+        './data/cats_and_dogs_small/test/cats/cat.1700.jpg'
+
+    img = image.load_img(img_path, target_size=(150, 150))
+    img_tensor = image.img_to_array(img)
+    img_tensor = np.expand_dims(img_tensor, axis=0)
+
+    img_tensor /= 255.
+
+    # 出力側の８つの層から出力を抽出
+    layer_outputs = [layer.output for layer in model.layers[:8]]
+    # 特定の入力をもとに、これらの出力を返すモデルを作成
+    activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+
+    # ５つのNumPy配列（層の活性化ごとに１つ）のリストを返す
+    activations = activation_model.predict(img_tensor)
+
+    # プロットの一部として使用する層の名前
+    layer_names = []
+    for layer in model.layers[:8]:
+        layer_names.append(layer.name)
+
+    images_per_row = 16
+
+    # 特徴マップを表示
+    for layer_name, layer_activation in zip(layer_names, activations):
+        # 特徴マップに含まれている特徴量の数
+        n_features = layer_activation.shape[-1]
+
+        # 特徴マップの形状(1, size, size, n_features)
+        size = layer_activation.shape[1]
+
+        # この行列で活性化のチャネルをタイル表示
+        n_cols = n_features // images_per_row
+        display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+        # 各フィルタを１つの大きな水平グリッドでタイル表示
+        for col in range(n_cols):
+            for row in range(images_per_row):
+                channel_image = layer_activation[0, :, :,
+                                                 col * images_per_row + row]
+                # 特徴量の見た目をよくするための後処理
+                channel_image -= channel_image.mean()
+                channel_image /= channel_image.std()
+                channel_image *= 64
+                channel_image += 128
+                channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+                display_grid[col * size : (col + 1) * size,
+                             row * size : (row + 1) * size] = channel_image
+
+        # グリッドを表示
+        scale = 1. / size
+        plt.figure(figsize=(scale * display_grid.shape[1],
+                            scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+    # plt.savefig("./fig/activations.png")
+    plt.show()
+
 
 if __name__ == '__main__':
-    showing_middle_layer_output()
+    showing_middle_layer_output_all()
 
