90eb183bc08c3fc23d35b2225e8a92b6ab3f05a7
monda <monda0524@gmail.com>
2019-04-21 16:12:17 +0900

    update 6-2 add simple RNN model

--- 6-2_understand_rnn.py
+++ 6-2_understand_rnn.py
@@ -3,6 +3,11 @@
 '''
 
 import numpy as np
+from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Embedding, SimpleRNN, Dense
+import matplotlib.pyplot as plt
 
 # 単純なRNNのNumPy実装
 def simple_rnn():
@@ -36,7 +41,77 @@ def simple_rnn():
     final_output_sequence = np.stack(successive_outputs, axis=0)
     print(final_output_sequence)
 
+# 結果をプロット
+def show_result(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+
+    # 正解率をプロット
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and Validation accuracy')
+    plt.legend()
+    plt.savefig('./fig/6-2_training_and_validation_accuracy.png')
+
+    plt.figure()
+
+    # 損失値をプロット
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and Validation loss')
+    plt.legend()
+    plt.savefig('./fig/6-2_training_and_validation_loss.png')
+
+# Embedding層とSimpleRNN層を使ったモデル
+def build_embedding_and_simplernn_model(max_features):
+    model = Sequential()
+    model.add(Embedding(max_features, 32))
+    model.add(SimpleRNN(32))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    return model
+
+# IMDbデータに対するRNN
+def rnn_for_imdb():
+    '''
+    IMDbデータの前処理
+    '''
+    max_features = 10000    # 特徴量として考慮する単語の数
+    max_len = 500           # この数の単語を残してテキストをカット
+    batch_size = 32
+
+    print('Loading data...')
+    (input_train, y_train), (input_test, y_test) = \
+        imdb.load_data(num_words=max_features)
+    print(len(input_train), 'train sequence')
+    print(len(input_test), 'test sequence')
+
+    print('Pad sequences (samples x time)')
+    input_train = sequence.pad_sequences(input_train, maxlen=max_len)
+    input_test = sequence.pad_sequences(input_test, maxlen=max_len)
+    print('input_train shape:', input_train.shape)
+    print('input_test shape:', input_test.shape)
+
+    model = build_embedding_and_simplernn_model(max_features)
+    model.summary()
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+    history = model.fit(input_train, y_train,
+                        epochs=10, batch_size=128, validation_split=0.2)
+
+    show_result(history)
+
 
 if __name__ == '__main__':
-    simple_rnn()
+    rnn_for_imdb()
 
