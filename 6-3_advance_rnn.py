ed20f917e3df1ccca0e91551eb715aaae0a4c9ab
monda <monda0524@gmail.com>
2019-04-28 14:56:19 +0900

    update 6-3 bidirectional LSTM model

--- 6-3_advance_rnn.py
+++ 6-3_advance_rnn.py
@@ -9,6 +9,8 @@ from matplotlib import pyplot as plt
 from keras.models import Sequential
 from keras import layers
 from keras.optimizers import RMSprop
+from keras.datasets import imdb
+from keras.preprocessing import sequence
 
 # 気象データセットのデータの調査
 def search_data():
@@ -242,7 +244,58 @@ def stack_gru_model_with_dropout(train_gen, val_gen, test_gen, lookback, step, f
 
     show_result(history)
 
+def main_imdb():
+    # 特徴量として考慮する単語の数
+    max_features = 10000
+
+    # max_features個の最も出現頻度の高い単語のうち、
+    # この数の単語を残してテキストをカット
+    max_len = 50
+
+    # データを読み込む
+    (x_train, y_train), (x_test, y_test) = \
+        imdb.load_data(num_words=max_features)
+
+    # シーケンスを逆向きにする
+    x_train = [x[::-1] for x in x_train]
+    y_test = [x[::-1] for x in x_test]
+
+    #シーケンスをパディングする
+    x_train = sequence.pad_sequences(x_train, maxlen=max_len)
+    x_test = sequence.pad_sequences(x_test, maxlen=max_len)
+
+    model = build_bidi_lstm_model(max_features)
+
+    history = model.fit(x_train, y_train,
+                        epochs=10,
+                        batch_size=128,
+                        validation_split=0.2)
+
+def build_lstm_model(max_features):
+    model = Sequential()
+    model.add(layers.Embedding(max_features, 128))
+    model.add(layers.LSTM(32))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    return model
+
+def build_bidi_lstm_model(max_features):
+    model = Sequential()
+    model.add(layers.Embedding(max_features, 128))
+    model.add(layers.Bidirectional(layers.LSTM(32)))
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy',
+                  metrics=['acc'])
+
+    return model
+
 
 if __name__ == '__main__':
-    main()
+    main_imdb()
 
