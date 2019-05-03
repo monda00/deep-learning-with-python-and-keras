353559f0928803c40abf0feee23be39c97cf3e0d
monda <monda0524@gmail.com>
2019-05-03 17:10:14 +0900

    update 7-1 multi output model

--- 7-1_functinal_api.py
+++ 7-1_functinal_api.py
@@ -7,7 +7,8 @@ from keras import layers
 from keras import Input
 import numpy as np
 
-def dubble_input_model():
+# 多入力モデル
+def multi_input_model():
     text_vocabulary_size = 10000
     question_vocabulary_size = 10000
     answer_vocabulary_size = 500
@@ -68,7 +69,56 @@ def dubble_input_model():
     model.fit({'text': text, 'question': question}, answers,
               epochs=10, batch_size=128)
 
+# 多出力モデル
+def multi_output_model():
+    vocabulary_size = 50000
+    num_income_groups = 10
+
+    posts_input = Input(shape=(None, ), dtype='int32', name='posts')
+    embedded_posts = layers.Embedding(256, vocabulary_size)(posts_input)
+    x = layers.Conv1D(128, 5, activation='relu')(embedded_posts)
+    x = layers.MaxPooling1D(5)(x)
+    x = layers.Conv1D(256, 5, activation='relu')(x)
+    x = layers.Conv1D(256, 5, activation='relu')(x)
+    x = layers.MaxPooling1D(5)(x)
+    x = layers.Conv1D(256, 5, activation='relu')(x)
+    x = layers.Conv1D(256, 5, activation='relu')(x)
+    x = layers.GlobalMaxPooling1D()(x)
+    x = layers.Dense(128, activation='relu')(x)
+
+    # 出力層に名前がついていることに注意
+    age_prediction = layers.Dense(1, name='age')(x)
+    income_prediction = layers.Dense(num_income_groups,
+                                     activation='softmax',
+                                     name='income')(x)
+    gender_prediction = layers.Dense(1, activation='sigmoid', name='gender')(x)
+    model = Model(posts_input,
+                  [age_prediction, income_prediction, gender_prediction])
+
+    model.compile(optimizer='rmsprop',
+                  loss=['mse',
+                        'categorical_crossentropy',
+                        'binary_crossentropy'],
+                  loss_weights=[0.25, 1., 10.])
+
+    # 上記と同じ（出力層に名前をつけている場合にのみ可能）
+    model.compile(optimizer='rmsprop',
+                  loss={'age': 'mse',
+                        'income': 'categorical_crossentropy',
+                        'gender': 'binary_crossentropy'},
+                  loss_weights={'age': 0.25, 'income': 1., 'gender': 10.})
+
+    # age_targets, income_targets, gender_targetsはNumPy配列と仮定
+    model.fit(posts, [age_targets, income_targets, gender_targets],
+              epochs=10, batch_size=64)
+
+    # 上記と同じ（出力層に名前をつけている場合にのみ可能）
+    model.fit(posts, {'age': age_targets,
+                      'income': income_targets,
+                      'gender': gender_targets},
+              epochs=10, batch_size=64)
+
 
 if __name__ == '__main__':
-    dubble_input_model()
+    multi_output_model()
 
